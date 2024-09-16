f3d011de03c8a497135c82ee20fd5afe722ba801
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-09-16 03:30:32 +0000

    source immigration

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+`AutoProp`
+----------
+Auto propagation system for complex neural networks of Luma Python library.
+
+"""
+
+from .graph import LayerNode, LayerGraph
+from .merge import MergeMode
