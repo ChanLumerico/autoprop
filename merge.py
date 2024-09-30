b3004992a656cb1414b70fd1a6be77281c187ad7
Chan Lee <150145948+ChanLumerico@users.noreply.github.com>
2024-09-30 09:35:05 +0000

    MergeMode.HADAMARD update

--- merge.py
+++ merge.py
@@ -62,8 +62,10 @@ class MergeMode(Enum):
                 return d_out
 
             case MergeMode.HADAMARD:
-                total_prod = np.prod(f_queue, axis=0)
-                prod_except_current = total_prod / f_queue[i]
+                prod_except_current = np.ones_like(f_queue[0])
+                for j in range(len(f_queue)):
+                    if j != i:
+                        prod_except_current *= f_queue[j]
                 return d_out * prod_except_current
 
             case MergeMode.AVG:
